350e6552ddfaaa4202aba6b4d5fe5e8fbe3169f3
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2024-11-05 15:02:44 +0530

    Add files via upload

--- Pay-Pal/DEV/variable.tf
+++ Pay-Pal/DEV/variable.tf
@@ -1,3 +1,57 @@
+# variable "project_id" {
+#   description = "The ID of the project"
+#   type        = string
+# }
+
+# variable "region" {
+#   description = "The region to deploy resources"
+#   type        = string
+# }
+
+# variable "snapshot_name" {
+#   description = "The name of the snapshot to use for VM images"
+#   type        = string
+# }
+
+# # ... (previous variables remain the same)
+
+# variable "vms" {
+#   description = "List of VMs to create with their associated disks"
+#   type = list(object({
+#     name         = string
+#     machine_type = string
+#     zone         = string   
+#     private_ip   = string
+#     disks = list(object({
+#       name    = string
+#       size_gb = number
+#       type    = string
+#     }))
+#   }))
+# }
+
+# # Remove the separate 'disks' variable if it's no longer needed
+# variable "network_name" {
+#   description = "The name of the network to use"
+#   type        = string
+# }
+
+# variable "subnet_name" {
+#   description = "The name of the subnetwork to use"
+#   type        = string
+# }
+
+# variable "service_account_email" {
+#   description = "The email of the service account to use for VMs"
+#   type        = string
+# }
+
+# variable "tags" {
+#   description = "Tags to apply to the VMs"
+#   type        = list(string)
+# }
+
+
 variable "project_id" {
   description = "The ID of the project"
   type        = string
@@ -8,28 +62,6 @@ variable "region" {
   type        = string
 }
 
-variable "snapshot_name" {
-  description = "The name of the snapshot to use for VM images"
-  type        = string
-}
-
-# ... (previous variables remain the same)
-
-variable "vms" {
-  description = "List of VMs to create with their associated disks"
-  type = list(object({
-    name         = string
-    machine_type = string
-    zone         = string   
-    disks = list(object({
-      name    = string
-      size_gb = number
-      type    = string
-    }))
-  }))
-}
-
-# Remove the separate 'disks' variable if it's no longer needed
 variable "network_name" {
   description = "The name of the network to use"
   type        = string
@@ -45,7 +77,27 @@ variable "service_account_email" {
   type        = string
 }
 
+variable "snapshot_name" {
+  description = "The name of the snapshot to use for VM images"
+  type        = string
+}
+
 variable "tags" {
   description = "Tags to apply to the VMs"
   type        = list(string)
+}
+
+variable "vms" {
+  description = "List of VMs to create with their associated disks"
+  type = list(object({
+    name         = string
+    machine_type = string
+    zone         = string
+    private_ip   = string
+    disks = list(object({
+      name    = string
+      size_gb = number
+      type    = string
+    }))
+  }))
 }
